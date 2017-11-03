569481f330cca72787e01fce812cf4044af77a72
Chris E. Kelley <chris@vetula.com>
2017-11-03 14:20:35 +0100

    cleanup of demo

--- dist/bundle.js
+++ dist/bundle.js
@@ -61769,7 +61769,6 @@ function getViewListItemLength( element ) {
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
-/* unused harmony export toAcasiWidget */
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__ckeditor_ckeditor5_editor_classic_src_classiceditor__ = __webpack_require__(113);
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__ckeditor_ckeditor5_enter_src_enter__ = __webpack_require__(265);
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__ckeditor_ckeditor5_typing_src_typing__ = __webpack_require__(266);
@@ -61836,8 +61835,6 @@ class EftouchWidget extends __WEBPACK_IMPORTED_MODULE_16__ckeditor_ckeditor5_cor
     return [ __WEBPACK_IMPORTED_MODULE_17__ckeditor_ckeditor5_widget_src_widget__["a" /* default */] ];
   }
 
-
-
   init() {
     const editor = this.editor;
     const doc = editor.document;
@@ -61849,20 +61846,10 @@ class EftouchWidget extends __WEBPACK_IMPORTED_MODULE_16__ckeditor_ckeditor5_cor
     // Configure schema.
     schema.registerItem( 'acasi' );
     schema.allow( { name: 'acasi', inside: '$root' } );
-    schema.allow( { name: '$text', inside: '$root' } );
-    // schema.allow( { name: 'div', inside: '$root' } );
-    schema.allow( { name: '$block', inside: 'acasi' } );
-    schema.allow( { name: 'image', inside: 'acasi' } );
-    schema.allow( { name: '$text', inside: 'acasi' } );
-    schema.allow( { name: 'paragraph', inside: 'acasi' } );
-    schema.allow( { name: 'paragraph', inside: 'figure' } );
-    schema.allow( { name: '$text', inside: 'figure' } );
-    // schema.allow( { name: 'div', inside: 'acasi' } );
+    schema.allow( { name: '$inline', inside: '$root' } );
+    schema.allow( { name: '$inline', inside: 'acasi' } );
     schema.objects.add( 'acasi' );
-    schema.objects.add( '$text' );
-    schema.objects.add( 'image' );
-    // schema.objects.add( 'paragraph' );
-    // schema.objects.add( 'div' );
+
 
     // Build converter from model to view for editing pipeline.
     Object(__WEBPACK_IMPORTED_MODULE_10__ckeditor_ckeditor5_engine_src_conversion_buildmodelconverter__["a" /* default */])().for( data.modelToView, editing.modelToView )
@@ -61878,9 +61865,6 @@ class EftouchWidget extends __WEBPACK_IMPORTED_MODULE_16__ckeditor_ckeditor5_cor
     Object(__WEBPACK_IMPORTED_MODULE_11__ckeditor_ckeditor5_engine_src_conversion_buildviewconverter__["a" /* default */])().for( data.viewToModel )
       .fromElement( 'acasi' )
       .toElement( () => new __WEBPACK_IMPORTED_MODULE_13__ckeditor_ckeditor5_engine_src_model_element__["a" /* default */]( 'acasi' ) );
-
-    // editor.commands.add( ACASI, new AttributeCommand( editor, ACASI ) );
-    // const command = editor.commands.get( 'acasi' );
 
     // Add acasi button to feature components.
     editor.ui.componentFactory.add( 'insertAcasi', locale => {
@@ -61902,32 +61886,8 @@ class EftouchWidget extends __WEBPACK_IMPORTED_MODULE_16__ckeditor_ckeditor5_cor
           const imageElement = new __WEBPACK_IMPORTED_MODULE_13__ckeditor_ckeditor5_engine_src_model_element__["a" /* default */]( 'image', {
             src: imageUrl
           } );
-          // editor.data.insertContent( imageElement, editor.document.selection );
-          // const figureElement = new ModelElement( 'figure', { class: 'fancy-widget' }, new ViewText( 'ACASI' ) );
-          // const figureElement = new ModelElement( 'figure', { class: 'fancy-widget' });
-          // const figureElement = new ModelElement( 'div', { data: 'ACASI'});
-          // const figureElement = new ModelElement( 'paragraph', { data: 'ACASI'}, new Text( 'foo' ) );
-          // const figureElement = new ModelElement( 'paragraph', { data: 'ACASI'},  new ModelElement( 'text') );
-          // const figureElement = new ModelElement( 'div', { data: 'ACASI'}, imageElement );
-          // const figureElement = new ModelElement( 'div', { 'class': 'editor', 'contentEditable': 'true' } );
-          // const widgetElement = new ViewContainerElement( 'figure', { class: 'fancy-widget' }, new ViewText( 'ACASI' ) );
-          // const widgetElement = new ModelElement( 'figure', { class: 'fancy-widget' }, new ViewText( 'ACASI' ) );
-
-          // const widget =  toWidget( widgetElement );
-          // editor.data.insertContent( figureElement, editor.document.selection );
-          // const widgetElement = new ViewContainerElement( 'figure', { class: 'widget' }, new ViewText( 'ACASI' ) );
-          const widgetElement = new __WEBPACK_IMPORTED_MODULE_13__ckeditor_ckeditor5_engine_src_model_element__["a" /* default */]('acasi', { class: 'fancy-widget' },imageElement)
+          const widgetElement = new __WEBPACK_IMPORTED_MODULE_13__ckeditor_ckeditor5_engine_src_model_element__["a" /* default */]('figure', { class: 'fancy-widget' },imageElement)
           editor.data.insertContent( widgetElement, editor.document.selection );
-
-
-
-          // editor.execute( 'acasi' )
-
-          // const tangyElement = new ModelElement( 'eftouch', {
-          //   src: imageUrl
-          // } );
-          // editor.data.insertContent( tangyElement, editor.document.selection );
-
         } );
       });
       return view;
@@ -61938,125 +61898,6 @@ class EftouchWidget extends __WEBPACK_IMPORTED_MODULE_16__ckeditor_ckeditor5_cor
 /* harmony export (immutable) */ __webpack_exports__["a"] = EftouchWidget;
 
 
-/**
- * Converts given {@link module:engine/view/element~Element} to widget in following way:
- * * sets `contenteditable` attribute to `true`,
- * * adds custom `getFillerOffset` method returning `null`,
- * * adds `ck-widget` CSS class,
- * * adds custom property allowing to recognize widget elements by using {@link ~isWidget},
- * * implements `addHighlight` and `removeHighlight` custom properties to handle view highlight on widgets.
- *
- * @param {module:engine/view/element~Element} element
- * @param {Object} [options={}]
- * @param {String|Function} [options.label] Element's label provided to {@link ~setLabel} function. It can be passed as
- * a plain string or a function returning a string.
- * @returns {module:engine/view/element~Element} Returns same element.
- */
-function toAcasiWidget( element, options = {} ) {
-  element.setAttribute( 'contenteditable', true );
-  // element.getFillerOffset = getFillerOffset;
-  element.getFillerOffset = null;
-  element.addClass( WIDGET_CLASS_NAME );
-  element.setCustomProperty( widgetSymbol, true );
-
-  if ( options.label ) {
-    setLabel( element, options.label );
-  }
-
-  setHighlightHandling(
-    element,
-    ( element, descriptor ) => element.addClass( ...normalizeToArray( descriptor.class ) ),
-    ( element, descriptor ) => element.removeClass( ...normalizeToArray( descriptor.class ) )
-  );
-
-  return element;
-
-  // Normalizes CSS class in descriptor that can be provided in form of an array or a string.
-  function normalizeToArray( classes ) {
-    return Array.isArray( classes ) ? classes : [ classes ];
-  }
-}
-
-// ClassicEditor.create( global.document.querySelector( '#editor2' ), {
-//   plugins: [ Enter, Typing, Paragraph, Undo, Heading, Bold, Italic, List, EftouchWidget ],
-//   toolbar: [ 'headings', 'undo', 'redo', 'bold', 'italic', 'numberedList', 'bulletedList', 'eftouch', 'showCode' ]
-// } )
-//   .then( editor => {
-//     window.editor = editor;
-//     if (typeof Tangy == 'undefined') {
-//       window.Tangy = new Object()
-//       window.Tangy.editor = editor
-//     } else {
-//       window.Tangy.editor = editor
-//     }
-//
-//     buildModelConverter()
-//       .for( editor.editing.modelToView )
-//       .fromMarker( 'marker' )
-//       .toHighlight( data => ( { class: 'highlight-' + data.markerName.split( ':' )[ 1 ] } ) );
-//
-//     document.getElementById( 'add-marker-yellow' ).addEventListener( 'mousedown', evt => {
-//       addMarker( editor, 'yellow' );
-//       evt.preventDefault();
-//     } );
-//
-//     document.getElementById( 'add-marker-blue' ).addEventListener( 'mousedown', evt => {
-//       addMarker( editor, 'blue' );
-//       evt.preventDefault();
-//     } );
-//
-//     document.getElementById( 'add-marker-red' ).addEventListener( 'mousedown', evt => {
-//       addMarker( editor, 'red' );
-//       evt.preventDefault();
-//     } );
-//
-//     document.getElementById( 'remove-marker-yellow' ).addEventListener( 'mousedown', evt => {
-//       removeMarker( editor, 'yellow' );
-//       evt.preventDefault();
-//     } );
-//
-//     document.getElementById( 'remove-marker-blue' ).addEventListener( 'mousedown', evt => {
-//       removeMarker( editor, 'blue' );
-//       evt.preventDefault();
-//     } );
-//
-//     document.getElementById( 'remove-marker-red' ).addEventListener( 'mousedown', evt => {
-//       removeMarker( editor, 'red' );
-//       evt.preventDefault();
-//     } );
-//
-//     document.getElementById( 'remove-markers' ).addEventListener( 'mousedown', evt => {
-//       const markers = editor.document.markers;
-//
-//       editor.document.enqueueChanges( () => {
-//         for ( const marker of markers ) {
-//           markers.remove( marker );
-//         }
-//       } );
-//
-//       evt.preventDefault();
-//     } );
-//   } )
-//   .catch( err => {
-//     console.error( err.stack );
-//   } );
-//
-// function addMarker( editor, color ) {
-//   const model = editor.document;
-//
-//   editor.document.enqueueChanges( () => {
-//     const range = ModelRange.createFromRange( model.selection.getFirstRange() );
-//     model.markers.set( 'marker:' + color, range );
-//   } );
-// }
-//
-// function removeMarker( editor, color ) {
-//   const model = editor.document;
-//
-//   editor.document.enqueueChanges( () => {
-//     model.markers.remove( 'marker:' + color );
-//   } );
-// }
 
 /***/ }),
 /* 511 */
